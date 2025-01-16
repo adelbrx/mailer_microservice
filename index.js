9de2f2915d6b2f5024594fec69fb61cd7f1f8753
unknown <adelbrixi1@gmail.com>
2025-01-16 16:56:34 +0200

    vercel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 // SETTING THE PORT NUMBER
-const port = 3000;
+const port = 8080 || process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
