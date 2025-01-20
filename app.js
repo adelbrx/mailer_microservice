e9e9cfef281e10a1b14c60717594abd388e6715a
unknown <adelbrixi1@gmail.com>
2025-01-20 22:11:47 +0200

    adding cors configuration

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const globalErrorHandler = require("./controllers/errorController");
 // INITIALIZE EXPRESS APP
 const app = express();
 
+// CORS Config
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
+
 // CONVERT JSON TO JAVASCRIPT OBJECT
 app.use(express.json());
 
