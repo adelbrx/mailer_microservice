667d4e484d5b1a5a8f6c540a647646aa2260266a
unknown <adelbrixi1@gmail.com>
2025-01-20 23:27:38 +0200

    cors

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "*",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "127.0.0.1",
       "localhost",
       "localhost:5173",
       "127.0.0.1:5173",
+      "*:5173",
+      "*",
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
